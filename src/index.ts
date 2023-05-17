a9f15b5a8501cdca688e18bb014f9d8aed9e7089
thelicato <thelicato@users.noreply.github.com>
2023-05-17 10:23:48 +0200

    feat: return stream for module usage

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { createStream, Options } from 'rotating-file-stream';
+
+export interface IPinoRotatingFileStreamOptions extends Options {
+  filename: string;
+  path: string;
+}
+
+export default (options: IPinoRotatingFileStreamOptions) => {
+  const { filename, size, maxSize, interval, compress, path } = options;
+  return createStream(filename, {
+    size: size || '100M',
+    maxSize: maxSize || '1G',
+    interval: interval || '7d',
+    compress: compress || 'gzip',
+    path: path,
+  });
+};
